9a66943c41aa2da7d3016a386ee893ccb1181add
CamiloP <juancamilopadillabulla@gmail.com>
2026-02-26 14:08:23 -0500

    Calculadora de IMC

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Declaracion de variables
+        double peso; // masa ingresada por el usuario en kg
+        double altura; // altura ingresada por el usuario en metros
+        double IMC; // indice de masa corporal, resultado de dividir peso / altura
+
+        // Comienzo algoritmo
+        Scanner teclado = new Scanner (System.in);
+
+        System.out.println("Bienvenido, este algoritmo le permitira saber su estado nutricional " +
+                "actual en base a su Indice de Masa Corporal (IMC)");
+        System.out.println("Porfavor ingrese su PESO en KILOGRAMOS (kg): ");
+        peso = teclado.nextDouble();
+        System.out.println("Porfavor ingrese su ALTURA en METROS (m) : ");
+        altura = teclado.nextDouble();
+
+        IMC = peso / (altura*altura);
+        System.out.println("Su IMC equivale a: "+ IMC);
+        System.out.println("En base a estos resultados, su estado de peso actual es de");
+
+        if (IMC > 0 || IMC < 18.5){
+            System.out.println("Peso bajo");
+        }
+        else if (IMC >= 18.5 || IMC <= 24.9){
+            System.out.println("Peso normal");
+        }
+        else if (IMC >= 25 || IMC <= 26.9){
+            System.out.println("Sobre peso grado 1");
+        }
+        else if (IMC >= 27 || IMC <= 29.9){
+            System.out.println("Sobre peso grado 2");
+        }
+        else if (IMC >= 30 || IMC <= 34.9){
+            System.out.println("Obecidad tipo 1");
+        }
+        else if (IMC >= 35 || IMC <= 39.9){
+            System.out.println("Obecidad tipo 2");
+        }
+        else if (IMC >= 40 || IMC <= 49.9){
+            System.out.println("Obecidad morbida");
+        }
+        else if (IMC >= 50){
+            System.out.println("Obecidad extrema");
+        }
+        else {
+            System.out.println("Los datos ingresados son inhumanos");
+        }
+    }
+}
