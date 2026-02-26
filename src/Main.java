842f90bbbebcb93605d4c4ecb749c3e409fb69ec
CamiloP <juancamilopadillabulla@gmail.com>
2026-02-26 14:21:29 -0500

    Calculadora de IMC v2

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         // Comienzo algoritmo
         Scanner teclado = new Scanner (System.in);
 
+        // Pide datos al usuario
         System.out.println("Bienvenido, este algoritmo le permitira saber su estado nutricional " +
                 "actual en base a su Indice de Masa Corporal (IMC)");
         System.out.println("Porfavor ingrese su PESO en KILOGRAMOS (kg): ");
@@ -18,29 +19,33 @@ public class Main {
         System.out.println("Porfavor ingrese su ALTURA en METROS (m) : ");
         altura = teclado.nextDouble();
 
+        // Calcula el IMC y lo imprime
         IMC = peso / (altura*altura);
-        System.out.println("Su IMC equivale a: "+ IMC);
-        System.out.println("En base a estos resultados, su estado de peso actual es de");
 
-        if (IMC > 0 || IMC < 18.5){
+        System.out.println("Su IMC equivale a: "+ IMC);
+
+        // Compara el valor de IMC, para decirle el estado de peso actual al usuario
+        System.out.println("En base a estos resultados, su estado de peso actual es de: \n");
+
+        if (IMC > 0 && IMC < 18.5){
             System.out.println("Peso bajo");
         }
-        else if (IMC >= 18.5 || IMC <= 24.9){
+        else if (IMC >= 18.5 && IMC <= 24.9){
             System.out.println("Peso normal");
         }
-        else if (IMC >= 25 || IMC <= 26.9){
-            System.out.println("Sobre peso grado 1");
+        else if (IMC >= 25 && IMC <= 26.9){
+            System.out.println("Sobrepeso grado 1");
         }
-        else if (IMC >= 27 || IMC <= 29.9){
-            System.out.println("Sobre peso grado 2");
+        else if (IMC >= 27 && IMC <= 29.9){
+            System.out.println("Sobrepeso grado 2");
         }
-        else if (IMC >= 30 || IMC <= 34.9){
+        else if (IMC >= 30 && IMC <= 34.9){
             System.out.println("Obecidad tipo 1");
         }
-        else if (IMC >= 35 || IMC <= 39.9){
+        else if (IMC >= 35 && IMC <= 39.9){
             System.out.println("Obecidad tipo 2");
         }
-        else if (IMC >= 40 || IMC <= 49.9){
+        else if (IMC >= 40 && IMC <= 49.9){
             System.out.println("Obecidad morbida");
         }
         else if (IMC >= 50){
